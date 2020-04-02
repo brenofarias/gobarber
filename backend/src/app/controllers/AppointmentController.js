24e7fe6e235307cb828fa6fcdeeb193d03dca930
Breno Farias Sousa <brenosousa.farias@outlook.com>
2020-04-02 12:06:51 -0300

    Validações de agendamento
    Validações de agendamento usando o date-fns

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from "yup";
+import { startOfHour, parseISO, isBefore } from "date-fns";
 import User from "../models/User";
 import Appointment from "../models/Appointment";
 
@@ -28,10 +29,38 @@ class AppointmentController {
         .json({ error: "Você só pode criar compromissos com fornecedores" });
     }
 
+    /**
+     * Checando datas passadas
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res
+        .status(400)
+        .json({ error: "Datas passadas não são permitidas" });
+    }
+
+    /**
+     * Checando disponibilidade de data
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart
+      }
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: "Data do compromisso não está disponível" });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date
+      date: hourStart
     });
 
     return res.json(appointment);
