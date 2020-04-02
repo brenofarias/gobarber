7ca4f51e576ce7e247124601af87ea4869c6e0ef
Breno Farias Sousa <brenosousa.farias@outlook.com>
2020-04-02 15:24:08 -0300

    Marcar notifações como lidas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -30,6 +30,7 @@ routes.get("/appoitments", AppointmentController.index);
 routes.get("/schedule", ScheduleController.index);
 
 routes.get("/notifications", NotificationController.index);
+routes.put("/notifications/:id", NotificationController.update);
 
 routes.post("/files", upload.single("file"), FileController.store);
 
